1160bb76972682481d74fc2aeeeb52b7da02b0bd
김정순_이모션 <jeongsoon.kim@emotion.co.kr>
2021-05-22 01:48:19 +0900

    본페이지 초기작업

--- js/common.js
+++ js/common.js
@@ -1,31 +1,30 @@
-
-$("document").ready(function() {
-//attr 현재속성
+$("document").ready(function () {
+    //attr 현재속성
 
     $(".tab_content").hide();
     $(".tab_content").eq(0).show();
 
 
-    $("ul.tabs li").click(function(){
+    $("ul.tabs li").click(function () {
         $("ul.tabs li").removeClass("active")
         $(this).addClass("active")
         $(".tab_content").hide();
         var tabid = $(this).attr("rel");
-        $("#"+tabid).fadeIn();
+        $("#" + tabid).fadeIn();
     })
 });
 
-$(function(){
-	$(".btn_event").click(function(){
-		$(".event1_txt_box").css("display","block");
-    $(".detail_group_area .event1_title_box a").addClass('on');
-	});
+$(function () {
+    $(".btn_event").click(function () {
+        $(".event1_txt_box").css("display", "block");
+        $(".detail_group_area .event1_title_box a").addClass('on');
+    });
 });
 
-$(function(){
-  var clipboard = new ClipboardJS('.hashtag_copy');
-  clipboard.on('success', function(e) {
-     alert('해시태그가 복사되었습니다.');
-  });
-	
-});
+// $(function () {
+//     var clipboard = new ClipboardJS('.hashtag_copy');
+//     clipboard.on('success', function (e) {
+//         alert('해시태그가 복사되었습니다.');
+//     });
+
+// });
